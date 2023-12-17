d42d670888f973df00f52e3b2bdfbe7bfc036188
carlos.henrique <carlos.henrique@timwe.com>
2023-12-17 00:23:54 -0300

    add UI usando Compose

--- app/src/main/java/com/chslcompany/dictionary/ui/MainActivity.kt
+++ app/src/main/java/com/chslcompany/dictionary/ui/MainActivity.kt
@@ -3,41 +3,93 @@ package com.chslcompany.dictionary.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.chslcompany.dictionary.feature_dictionary.presentation.WordInfoItem
+import com.chslcompany.dictionary.feature_dictionary.presentation.viewmodel.WordInfoViewModel
 import com.chslcompany.dictionary.ui.theme.DictionaryTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DictionaryTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+               val viewModel : WordInfoViewModel = hiltViewModel()
+               val state = viewModel.state.value
+               val scaffoldState = rememberScaffoldState()
+
+               LaunchedEffect(key1 = true){
+                   viewModel.eventFlow.collectLatest { event ->
+                       when(event){
+                           is WordInfoViewModel.UiEvent.ShowSnackBar ->{
+                               scaffoldState.snackbarHostState.showSnackbar(
+                                   message = event.message
+                               )
+                           }
+                       }
+                   }
+               }
+                Scaffold(
+                  scaffoldState = scaffoldState
+                ) { paddingValue ->
+                    Box(
+                        modifier = Modifier
+                            .background(MaterialTheme.colors.background)
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(paddingValue)
+                        ) {
+                            TextField(
+                                value = viewModel.searchQuery.value,
+                                onValueChange = viewModel::onSearch,
+                                modifier = Modifier.fillMaxWidth(),
+                                placeholder = {
+                                    Text(text = "Pesquise...")
+                                }
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            LazyColumn(
+                                modifier = Modifier.fillMaxSize()
+                            )
+                            {
+                                items(state.wordInfoItems.size){ position->
+                                   val wordInfo = state.wordInfoItems[position]
+                                   if (position > 0){
+                                       Spacer(modifier = Modifier.height(8.dp))
+                                   }
+                                   WordInfoItem(wordInfo = wordInfo)
+                                    if (position < state.wordInfoItems.size - 1){
+                                        Divider()
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DictionaryTheme {
-        Greeting("Android")
-    }
-}
